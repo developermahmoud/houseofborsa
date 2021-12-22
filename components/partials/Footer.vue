92f8fe844b858a9f516721b757424c8102432580
Mahmoud Ahmed <developer.mahmoudahmed@gmail.com>
2021-12-22 17:59:17 +0200

    fix btn

--- components/partials/Footer.vue
+++ components/partials/Footer.vue
@@ -4,26 +4,27 @@
       fixed
       grow
       :value="value"
+      height="70"
       color="primary"
       class="secondaryBackground"
     >
-      <v-btn>
+      <v-btn height="70">
         <span>Home</span>
         <v-icon>fa-home</v-icon>
       </v-btn>
-       <v-btn>
+      <v-btn height="70">
         <span>Wallet</span>
         <v-icon>fa-wallet</v-icon>
       </v-btn>
-      <v-btn>
+      <v-btn height="70">
         <span>Markets</span>
         <v-icon>fa-search-dollar</v-icon>
       </v-btn>
-       <v-btn>
+      <v-btn height="70">
         <span>Community</span>
         <v-icon>fa-users</v-icon>
       </v-btn>
-      <v-btn>
+      <v-btn height="70">
         <span>Support</span>
         <v-icon>fa-question-circle</v-icon>
       </v-btn>
