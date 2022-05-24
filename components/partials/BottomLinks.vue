f357ae5a4c2f64812ae0bda776830d07fa08e938
Mahmoud Ahmed <developer.mahmoudahmed@gmail.com>
2022-05-24 12:28:02 +0200

    upldao

--- components/partials/BottomLinks.vue
+++ components/partials/BottomLinks.vue
@@ -1,16 +1,57 @@
 <template>
   <div>
+    <div>
+      <div class="body-2 primary--text">LEGAL INFORMATION:</div>
+      <p class="caption">
+        House Of Borse Limited is authorized and regulated by the Financial
+        Conduct Authority, under Firm Reference Number (FRN) 631382. House Of
+        Borse is a company registered in England and Wales under registration
+        number: 09137156. Registered Office: Berkeley Square House, 2nd Floor,
+        Berkeley Square, Mayfair, London, UK.
+      </p>
+      <div class="body-2 red--text">RISK WARNING:</div>
+      <p class="caption">
+        CFDs and FX are complex instruments and come with a high risk of losing
+        money rapidly due to leverage. You should consider whether you
+        understand how CFDs work and whether you can afford to take the high
+        risk of losing your money. Professional clients can lose more than they
+        deposit. All trading involves risk. You should not speculate with
+        capital that you cannot afford to lose. Past performance in Financial
+        markets is not a reliable indicator of future results. Please consider
+        our Risk Disclosure , Privacy Policy and our Terms of Use before using
+        our services. If you are still unsure if investing is right for you,
+        please seek independent advice. HOB assumes no liability for any loss
+        sustained from trading in accordance with a recommendation.
+      </p>
+      <p class="caption">
+        The Domain name is fully registered and owned by House of Borse Limited.
+        You must be over the age of 18 to open an account with House of Borse
+        Limited. Disclaimer: The information on this site is not directed at
+        residents of the United States and is not intended for distribution to,
+        or use by, any person in any country or jurisdiction where such
+        distribution or use would be contrary to local law or regulation.
+      </p>
+      <p class="caption">
+        House of Borse records all phone calls for monitoring and regulatory
+        purposes
+      </p>
+      <p class="caption">
+        The Apple, Mac and iTunes logo are trademarks of Apple, registered in
+        the US and other countries. App Store is a service mark of Apple Inc.
+        Android is a trademark of Google, registered in the US and other
+        countries. Windows is a trademark of Microsoft, registered in the US and
+        other countries.
+      </p>
+    </div>
     <div class="text-center">
       <div class="d-flex align-center justify-center">
-        <img src="/hob.jpeg" style="width: 20px;" />
+        <img src="/hob.jpeg" style="width: 20px" />
         <v-icon small class="mx-2">mdi-facebook</v-icon>
         <v-icon small class="mx-2">mdi-twitter</v-icon>
         <v-icon small class="mx-2">mdi-youtube</v-icon>
         <v-icon small>mdi-instagram</v-icon>
       </div>
-      <div class="grey--text caption">
-        @copy; 2022 HOB
-      </div>
+      <div class="grey--text caption">@copy; 2022 HOB</div>
     </div>
   </div>
 </template>
