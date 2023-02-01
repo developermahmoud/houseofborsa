e65d1b5c67672d17aa3afa621de4eb3e5f4cba2a
Mahmoud Ahmed <developer.mahmoudahmed@gmail.com>
2023-02-01 11:00:49 +0200

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,31 @@
 <template>
- <v-app dark class="background"> 
-     <top-navbar />   
-      <v-main class="background">
-        <v-container>
-           <Nuxt />
-        </v-container>
-      </v-main>
-      <Footer />
+  <v-app dark class="background">
+    <top-navbar />
+    <v-main class="background">
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <Footer />
+    <v-dialog v-model="isDisable" max-width="500" persistent>
+      <v-card class="secondary">
+        <v-card-title class="justify-center">Login</v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="password"
+            outlined
+            hide-details
+            type="password"
+            label="Enter password"
+          >
+          </v-text-field>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn class="primary" @click="login" text block tile>Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
@@ -16,7 +35,17 @@ import Footer from "../components/partials/Footer.vue";
 export default {
   components: { TopNavbar, Footer },
   data() {
-    return {};
+    return {
+      isDisable: true,
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      if (this.password == "Quantex") {
+        this.isDisable = false;
+      }
+    },
   },
 };
 </script>
